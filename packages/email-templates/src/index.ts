3e89a5feb30281e72758c2f07003dc634fd13dc5
Ha Doan <ha.doanmanh@gmail.com>
2025-12-23 00:06:16 +0100

    cleanup

--- packages/email-templates/src/index.ts
+++ packages/email-templates/src/index.ts
@@ -1,2 +1,6 @@
 export { renderEmail } from "./render.js";
 export type { RenderEmailResult } from "./render.js";
+
+// Re-export invoices module
+export { InvoiceEmail, buildInvoiceEmailSubject } from "./invoices/index.js";
+export type { InvoiceEmailProps } from "./invoices/index.js";
